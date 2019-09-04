ff499040baf13a477dbd49a23e4ab70f32800f3c
Ioannis <olgagian@otenet.gr>
2019-09-04 15:03:33 +0300

    another git test

--- Market/Market/AppDelegate.swift
+++ Market/Market/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         FirebaseApp.configure()
         //testing one two three
+        //testing one two three//testing one two three//testing one two three//testing one two three//testing one two three//testing one two three//testing one two three//testing one two three//testing one two three//testing one two three//testing one two three//testing one two three//testing one two three//testing one two three//testing one two three
         return true
     }
 
