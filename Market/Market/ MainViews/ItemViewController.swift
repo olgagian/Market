182f45b0284a4ed45d4f46c4ebbd6c48073407e5
Ioannis <olgagian@otenet.gr>
2019-09-05 19:27:49 +0300

    Added item detail with collection view as well as basket button

--- Market/Market/ MainViews/ItemViewController.swift
+++ Market/Market/ MainViews/ItemViewController.swift
@@ -17,11 +17,80 @@ class ItemViewController: UIViewController {
     var item: Item!
     var itemImages: [UIImage] = []
     var hdu = JGProgressHUD(style: .dark)
+    private let sectionInSets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0, right: 0.0)
+    private let cellHeight: CGFloat = 196.0
+    private let itemsPerRow:CGFloat = 1
+       
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
-    
+        setupUI()
+        downloadPictures()
+        
+        self.navigationItem.leftBarButtonItems=[UIBarButtonItem(image: UIImage(named: "back"), style: .plain, target: self , action:#selector(self.backAction))]
+        self.navigationItem.rightBarButtonItems=[UIBarButtonItem(image: UIImage(named: "basket"), style: .plain, target: self , action:#selector(self.addtoBasketButtonPressed))]
+    }
+    //MARK: - Download Pictures
+    private func downloadPictures() {
+        if item != nil && item.imageLinks != nil {
+            downloadImages(imageUrls: item.imageLinks) { (allImages) in
+                if allImages.count  > 0 {
+                    self.itemImages = allImages as! [UIImage]
+                    self.imageCollectionView.reloadData()
+                    
+                }
+            }
+        }
     }
     
+    private func setupUI() {
+        if item != nil {
+            self.title = item.name
+            nameLabel.text = item.name
+            priceLabel.text = convertToCurrency(item.price)
+            descriptionTextView.text = item.description
+            
+            
+        }
+        
+    }
+    //MARK- IBactions
+    
+    @objc func backAction() {
+        self.navigationController?.popViewController(animated: true)
+        
+    }
+    @objc func  addtoBasketButtonPressed() {
+        print("Add to basket",item.name)
+    }
+    
+}
+extension ItemViewController: UICollectionViewDataSource,UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return itemImages.count == 0 ? 1: itemImages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell  = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! ImageCollectionViewCell
+        if itemImages.count > 0 {
+        cell.setupImageWith(itemImage: itemImages[indexPath.row])
+        }
+            return cell
+    }
+    
+    
+    
+    
+}
+extension ItemViewController : UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let availableWidth = collectionView.frame.width - sectionInSets.left
+        return CGSize(width: availableWidth, height: cellHeight)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInSets
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInSets.left
+    }
 }
