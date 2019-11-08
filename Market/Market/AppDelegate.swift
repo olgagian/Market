be9ab3f507a26f33a756bf8f3bdfab19bd10c796
Ioannis <olgagian@otenet.gr>
2019-11-08 18:42:19 +0200

    Paypal initialization

--- Market/Market/AppDelegate.swift
+++ Market/Market/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-       
+       initializePayPal()
         return true
     }
 
@@ -34,7 +34,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
+//MATRK- paypal init
+    func initializePayPal() {
+        PayPalMobile.initializeWithClientIds(forEnvironments: [PayPalEnvironmentProduction : "ARsnPtSsZTqVWq4O1RN2mPEFc8if2GUeEBOHy5vnxdMFXm8DUHcX3vT5F6Wmkd5d3b1fvbELAuH29CmX",PayPalEnvironmentSandbox:"sb-zbxev75972@personal.example.com"])
+    }
 
 }
 
